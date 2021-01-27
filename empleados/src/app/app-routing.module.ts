c9d235391ecbe3c046b518963f46c1ca908503f2
jonathangonzalezlpz <jonathan.gonzalezlpz@gmail.com>
2021-01-27 17:04:12 +0100

    Inicio Configuracion Firebase

--- empleados/src/app/app-routing.module.ts
+++ empleados/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'list-empleados', pathMatch: 'full' }, //Establecemos una ruta por defecto para el caso en que no se indique un path
+  {path: 'list-empleados', component: ListEmpleadosComponent},
+  {path: 'create-empleado', component: CreateEmpleadoComponent},
+  {path: '**', redirectTo: 'list-empleados', pathMatch:'full'} //Caso de path no válido, IMPORTANTE COLOCAR AL FINAL
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
